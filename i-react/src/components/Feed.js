f6f2368eb54808451977471701016b82d07174ae
juangpu <juangabriel.urrutia@outlook.com>
2020-09-16 21:53:13 -0300

    transformando componentes statefull

--- i-react/src/components/Feed.js
+++ i-react/src/components/Feed.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Post from './Post'
-
-function Feed() {
-  return (
-    <div>
-      <Post  post="Post 1" qtdComentarios={10} />
-      <Post listaDeComentarios={["Comentario 1 do Post 2", "Comentario 2 do Post 2"]} post="Post 2" qtdComentarios={120}/>
-      <Post listaDeComentarios={["Comentario 1 do Post 3", "Comentario 2 do Post 3"]} post="Post 3" qtdComentarios={140}/>
-      <Post listaDeComentarios={["Comentario 1 do Post 4", "Comentario 2 do Post 4"]} post="Post 4" qtdComentarios={160}/>  
-    </div>
-  )
+// COMPONENTE STATEFULL
+class Feed extends Component {
+  render () {
+    return (
+      <div>
+        <Post id="post-1" post="Post 1" qtdComentarios={10} />
+        <Post id="post-2" listaDeComentarios={["Comentario 1 do Post 2", "Comentario 2 do Post 2"]} post="Post 2" qtdComentarios={120}/>
+        <Post id="post-3" listaDeComentarios={["Comentario 1 do Post 3", "Comentario 2 do Post 3"]} post="Post 3" qtdComentarios={140}/>
+        <Post id="post-4" listaDeComentarios={["Comentario 1 do Post 4", "Comentario 2 do Post 4"]} post="Post 4" qtdComentarios={160}/>  
+      </div>
+    )
+  }
 }
 
 export default Feed;
