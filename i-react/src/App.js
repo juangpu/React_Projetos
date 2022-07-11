b132c2121a03d54f0e184076309376aa13f46197
juangpu <juangabriel.urrutia@outlook.com>
2022-07-11 02:25:51 -0300

    Adicionando novo projeto

--- i-react/src/App.js
+++ i-react/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-// import Feed from './components/Feed'
-// import Anuncio from './components/Anuncio'
-
+import Feed from './components/Feed'
+import Anuncio from './components/Anuncio'
+import Post from './components/Post';
 import Contador from './components/Contador';
 
 function App() {
   return (
     <div className="App">
-      {/* <Anuncio>
+      <Anuncio>
         Sou o anuncio 1.
       </Anuncio>
       <Feed />
       <Anuncio>
         Sou o anuncio 2.
-      </Anuncio> */}
+      </Anuncio>
       <Contador />
     </div>
   );
